d4357f29758382ca66f5f6f3e3d9c460bb783788
Tugay Türk <i.tugayturk@gmail.com>
2026-01-10 03:08:44 +0300

    Logout added

---
+++ frontend/src/components/Menu/UserMenu.tsx
@@ -0,0 +1,67 @@
+"use client"
+import {
+    UserIcon,
+} from "lucide-react"
+import { Button } from "@/components/ui/button"
+import {
+    DropdownMenu,
+    DropdownMenuContent,
+    DropdownMenuItem,
+    DropdownMenuLabel,
+    DropdownMenuSeparator,
+    DropdownMenuTrigger,
+} from "@/components/ui/dropdown-menu"
+import { useRouter } from "next/navigation"
+import useAuthStore from "@/hooks/useAuth"
+import { useEffect } from "react"
+
+const UserMenu = () => {
+    const router = useRouter()
+    const { jwt, setJwt } = useAuthStore()
+
+    useEffect(() => {
+        if (typeof window !== "undefined") {
+            const user = localStorage.getItem("user")
+            const jwt = localStorage.getItem("jwt")
+            if (jwt && user) {
+                const userData = JSON.parse(user)
+                setJwt(jwt)
+            }
+
+        }
+    }, [])
+
+    const signOut = () => {
+        if (typeof window !== "undefined") {
+            localStorage.removeItem("user")
+            localStorage.removeItem("jwt")
+            setJwt("")
+            router.push("/login")
+        }
+    }
+
+    return (
+        <div>
+            {jwt ? (
+                <DropdownMenu>
+                    <DropdownMenuTrigger asChild>
+                        <Button variant="outline" >
+                            <UserIcon />
+                        </Button>
+                    </DropdownMenuTrigger>
+                    <DropdownMenuContent className="w-56" align="start">
+                        <DropdownMenuLabel>My Account</DropdownMenuLabel>
+                        <DropdownMenuItem>
+                            Profile
+                        </DropdownMenuItem>
+                        <DropdownMenuSeparator />
+                        <DropdownMenuItem onClick={() => signOut()}>
+                            Log out
+                        </DropdownMenuItem>
+                    </DropdownMenuContent>
+                </DropdownMenu>) : (<UserIcon/>)}
+        </div>
+    )
+}
+
+export default UserMenu
--- frontend/src/components/layout/Header.tsx
+++ frontend/src/components/layout/Header.tsx
@@ -1,41 +1,45 @@
 import Link from 'next/link'
 import { Input } from "@/components/ui/input"
 import {
-    UserIcon,
-    ShoppingCart,
-    SearchIcon
-  } from "lucide-react"
-  import { ModeToggle } from "@/components/ModeToggle"
-  import Image from "next/image";
+  UserIcon,
+  ShoppingCart,
+  SearchIcon
+} from "lucide-react"
+import { ModeToggle } from "@/components/ModeToggle"
+import Image from "next/image";
+import UserMenu from '../Menu/UserMenu'
+import { Button } from '../ui/button';
 
-
-  const Header = () => {
+const Header = () => {
   return (
     <header className="fixed top-0 left-0 w-full h-24 z-50 border-b-2 bg-gray-200 dark:bg-gray-900 overflow-visible">
       <div className="container mx-auto px-4 py-4 flex items-center justify-between overflow-visible">
-        
+
         <div className="flex items-center justify-between">
-        <Link href="/" className="flex items-center">
-        <Image
-          src="/logo.png"
-          alt="Site Logo"
-          width={60}
-          height={40}
-          priority
-          className='mb-2'
-        />
-      </Link>
+          <Link href="/" className="flex items-center">
+            <Image
+              src="/logo.png"
+              alt="Site Logo"
+              width={60}
+              height={40}
+              priority
+              className='mb-2'
+            />
+          </Link>
         </div>
 
         <div className="relative w-2/3 max-w-md">
-        <Input type="email" placeholder="Search" className="pr-10 w-full focus-visible:ring-0 focus-visible:ring-offset-0 focus-visible:border-input bg-white" />
-        <SearchIcon className='absolute right-3 top-1/2 -translate-y-1/2 w-4 h-4 pointer-events-none z-10' />
+          <Input type="email" placeholder="Search" className="pr-10 w-full focus-visible:ring-0 focus-visible:ring-offset-0 focus-visible:border-input bg-white" />
+          <SearchIcon className='absolute right-3 top-1/2 -translate-y-1/2 w-4 h-4 pointer-events-none z-10' />
         </div>
 
         <div className="flex items-center gap-2 w-1/4 justify-end ">
-        <UserIcon />
-        <ShoppingCart  />
-        <ModeToggle/>
+          <UserMenu />
+          <Button variant="outline" >
+            <ShoppingCart className="w-4 h-4" />
+          </Button>
+
+          <ModeToggle />
         </div>
       </div>
     </header>
